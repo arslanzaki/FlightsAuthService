5b166d2b3f0a665aa0e6a4d0e52eef33cbb0c67c
arslanzaki <arslanzaki223@gmail.com>
2023-10-15 21:57:48 +0500

    Added JWT Create & Verify Logic

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,8 +1,9 @@
 const dotenv = require("dotenv");
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT,
-  SALT: bcrypt.genSaltSync(10)
+  SALT: bcrypt.genSaltSync(10),
+  JWT_KEY: process.env.JWT_KEY,
 };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 const apiRoutes = require("./routes/index");
 
-const UserRepository = require("./repository/user-repository");
+// const UserRepository = require("./repository/user-repository");
+// const UserService = require("./services/user-service");
 const app = express();
 
 const prepareAndStartServer = () => {
@@ -12,9 +13,16 @@ const prepareAndStartServer = () => {
   app.use("/api", apiRoutes);
   app.listen(PORT, async () => {
     console.log(`Server Started On PORT ${PORT}`);
-    const repo = new UserRepository();
-    const response = await repo.getById(2);
-    console.log(response);
+    // const repo = new UserRepository();
+    // const response = await repo.getById(2);
+    // console.log(response);
+    // const service = new UserService();
+    // const newToken = service.createToken({ email: "test@testmail.com", id: 5 });
+    // console.log("New Token Is: ", newToken);
+    // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAdGVzdG1haWwuY29tIiwiaWQiOjUsImlhdCI6MTY5NzM4ODU5NiwiZXhwIjoxNjk3MzkyMTk2fQ.pXKvuaUmt2eRhu2sJh2-SPESZnwr4MJoP8_NnfAZuDw"
+    // const response = service.verifyToken(token);
+    // console.log("Response Is ", response)
+
   });
 };
 
